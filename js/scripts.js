528311db7d984d102b0abe9b5d9c6140753a23f4
zbl14 <ifthereisoneday@gmail.com>
2022-06-11 15:11:40 -0700

    revise forEach with map

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function wordPuzzle(text) {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  const textArr = text.split("");
+  const puzzleArr =textArr.map(function(letter) {
+    if (vowels.includes(letter)) {
+      return "-";
+    } else {
+      return letter;
+    }
+  });
+  return puzzleArr.join("");
+};
+
+// function wordPuzzle(text) {
+//   const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+//   const textArr = text.split("");
+//   let newArr = [];
+//   textArr.forEach(function(letter) {
+//     if (vowels.includes(letter)) {
+//       newArr.push("-");
+//     } else {
+//       newArr.push(letter);
+//     }
+//   });
+//   return newArr.join("");
+// }
