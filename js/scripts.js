ca3c7feba04ea5817b8ad9a40dadd27b2ba5dd9f
zbl14 <ifthereisoneday@gmail.com>
2022-06-11 15:19:10 -0700

    revise funchtion wit regex

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 function wordPuzzle(text) {
-  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  const textArr = text.split("");
-  const puzzleArr =textArr.map(function(letter) {
-    if (vowels.includes(letter)) {
-      return "-";
-    } else {
-      return letter;
-    }
-  });
-  return puzzleArr.join("");
-};
+  return text.replace(/[aeiou]/gi,"-") //gi:case insensitve, g: case sensitive
+}
+
+// function wordPuzzle(text) {
+//   const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+//   const textArr = text.split("");
+//   const puzzleArr =textArr.map(function(letter) {
+//     if (vowels.includes(letter)) {
+//       return "-";
+//     } else {
+//       return letter;
+//     }
+//   });
+//   return puzzleArr.join("");
+// };
 
 // function wordPuzzle(text) {
 //   const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
